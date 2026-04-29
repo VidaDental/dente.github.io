9f56477f66d14df080b91b96b36621e9d7197013
VidaDental <alexandraaky1998@yahoo.com>
2026-04-29 11:54:58 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -370,24 +370,3 @@ window.addEventListener("DOMContentLoaded", () => {
         }, 2000); 
     }
 });
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    // sub 640px: 1 slide
-    640: { slidesPerView: 2, spaceBetween: 20 },
-    // sub 1024px: 3 slide-uri
-    1024: { slidesPerView: 3, spaceBetween: 20 },
-    // peste 1024px: 4 slide-uri
-    1200: { slidesPerView: 4, spaceBetween: 25 }, 
-  },
-});
